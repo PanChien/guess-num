5deff1943c7cdafdfc8ead29ac8129d7fcb092b5
PanChien <zoo0605@hotmail.com>
2020-10-20 13:14:21 +0800

    finial version

--- guess-num.py
+++ guess-num.py
@@ -5,7 +5,12 @@
 
 import random
 
-r = random.randint(1, 100)
+start = input('請決定隨機數字範圍開始值：')
+end = input('請決定隨機數字結束值：')
+start = int(start)
+end = int(end)
+
+r = random.randint(start, end)
 
 count = 0	#計數用
 
@@ -15,6 +20,7 @@ while True:
 	count += 1  # count = count + 1
 	if num == r:
 		print('你猜中了!!')
+		print('你共猜了第', count, '次')
 		break
 	elif num > r:
 		print('比答案大。')
